2bfb36ae7a2c3983736d54c8e99a26b25d6ce471
Fábio Miguel de Andrade <fm.andrade@unifesp.br>
2026-05-10 09:21:56 -0300

    Add files via upload

--- MergeSort.c
+++ MergeSort.c
@@ -9,18 +9,33 @@
 #include <stdlib.h>
 
 void MERGE(int Array[], int p, int q, int r){
-    int n1 = q - p;
-    int n2 = r - q + 1;
+    int n1 = q - p + 1;
+    int n2 = r - q;
 
-    int L[n1], R[n2];
+    int L[n1 + 1], R[n2 + 1];
 
     for(int i = 0; i < n1; i++){
-        L[i] = Array[i];
+        L[i] = Array[p + i];
     }
 
-    //Concluir amanhã
     for(int j = 0; j < n2; j++){
+        R[j] = Array[q + 1 + j];
+    }
 
+    L[n1] = 100000;
+    R[n2] = 100000;
+
+    int i = 0;
+    int j = 0;
+
+    for (int k = p; k <= r; k++){
+        if(L[i] < R[j]){
+            Array[k] = L[i];
+            i = i + 1;
+        }else{
+            Array[k] = R[j];
+            j = j + 1;
+        }
     }
 }
 
